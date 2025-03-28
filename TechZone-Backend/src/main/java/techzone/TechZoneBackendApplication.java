3dddb4c34ddf7a7d2f42be43e8cd41a273b448de
Asil Galan <asilgalan083@gmail.com>
2025-03-28 13:26:38 +0100

    Inicialización del repositorio con backend y frontend

---
+++ TechZone-Backend/src/main/java/techzone/TechZoneBackendApplication.java
@@ -0,0 +1,13 @@
+package techzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechZoneBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechZoneBackendApplication.class, args);
+    }
+
+}
